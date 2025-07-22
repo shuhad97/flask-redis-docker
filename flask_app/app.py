60b64a852a63eae140bcbe1ac13bda90a612774d
shuhad97 <30422131+shuhad97@users.noreply.github.com>
2025-07-22 18:58:31 +0100

    Docker compose and counter added to app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from flask_redis import FlaskRedis
 
 
 app = Flask(__name__)
+
+
+app.config['REDIS_URL'] = 'redis://redis:6379/0'
+
+redis_client = FlaskRedis(app)
 
 
 @app.route('/')
 def welcome_message():
 
     return 'Welcome to Redis and Flask App!'
+
+@app.route('/count')
+def count():
+    redis_client.incr('hits')
+    count = redis_client.get('hits')
+
+    return f'This page has been visited {count.decode()} times'
 
 
 if __name__ == "__main__":
